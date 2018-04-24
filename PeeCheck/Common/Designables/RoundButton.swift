ff2d9c9c0c27b9af37590c24bab745f232c1b7a2
Martin <martin.lukacs@epitech.eu>
2018-04-25 01:03:11 +0200

    added test for round button

--- PeeCheck/Common/Designables/RoundButton.swift
+++ PeeCheck/Common/Designables/RoundButton.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 @IBDesignable
-class RoundButton: UIButton {
+final class RoundButton: UIButton {
 
     @IBInspectable var roundButton: Bool = false {
         didSet {
---
+++ PeeCheckTests/New Group/Designables/RoundButtonTests.swift
@@ -0,0 +1,42 @@
+//
+//  RoundButton.swift
+//  PeeCheckTests
+//
+//  Created by Martin Lukacs on 25/04/2018.
+//  Copyright © 2018 Martin Lukacs. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import PeeCheck
+
+class RoundButtonTests: QuickSpec {
+    
+    override func spec() {
+        describe("TabBarControllers enum Test") {
+            
+            it("should have corner radius equal to half frame height") {
+                let roundButton = RoundButton()
+                roundButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+                roundButton.roundButton = true
+                expect(roundButton.layer.cornerRadius) == roundButton.frame.height/2
+            }
+            
+            it("should not have corner radius equal to half frame height") {
+                let roundButton = RoundButton()
+                roundButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+                roundButton.roundButton = false
+                expect(roundButton.layer.cornerRadius) != roundButton.frame.height/2
+                expect(roundButton.layer.cornerRadius) == 0
+            }
+            
+            it("should have corner radius equal when prepared for interface builder called") {
+                let roundButton = RoundButton()
+                roundButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+                roundButton.roundButton = true
+                roundButton.prepareForInterfaceBuilder()
+                expect(roundButton.layer.cornerRadius) == roundButton.frame.height/2
+            }
+        }
+    }
+}
